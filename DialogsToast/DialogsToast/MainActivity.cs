f57778e35d3ab7e5af24c8bcffea05ef9c8161a0
cheahengsoon <cheahengsoon>
2016-09-25 23:20:32 +0800

    Xamarin Android for Beginner
    Layout , Dialog, Plugin

---
+++ DialogsToast/DialogsToast/MainActivity.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DialogsToast
+{
+    [Activity(Label = "DialogsToast", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button1 = FindViewById<Button>(Resource.Id.button1);
+            var button2 = FindViewById<Button>(Resource.Id.button2);
+            var button3 = FindViewById<Button>(Resource.Id.button3);
+
+            button1.Click += AlertClick1;
+            button2.Click += AlertClick2;
+            button3.Click+=AlertClick3;
+
+        }
+
+       
+        private void AlertClick1(object sender, EventArgs e)
+        {
+            Android.App.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog alertDialog = builder.Create();
+            alertDialog.SetTitle("Alert Title");
+            alertDialog.SetIcon(Android.Resource.Drawable.IcDialogAlert);
+            alertDialog.SetMessage("This is a sample message");
+            alertDialog.SetButton("OK", (s, ev) =>
+                {
+                    //Do Something
+                    //Only one Button Show
+                });
+            alertDialog.Show();
+
+        }
+        private void AlertClick3(object sender, EventArgs e)
+        {
+            Android.App.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog alertDialog = builder.Create();
+            alertDialog.SetTitle("Alert Title");
+            alertDialog.SetIcon(Android.Resource.Drawable.IcDialogAlert);
+            alertDialog.SetMessage("This is a sample message");
+          //YES
+            alertDialog.SetButton("YES",
+                (s, ev) =>
+            {
+                //Do Something
+                //Only one Button Show
+            });
+            alertDialog.SetButton("No",
+               (s, ev) =>
+               {
+                   //Do Something
+                   //Only one Button Show
+               });
+            alertDialog.Show();
+        }
+
+
+
+        private void AlertClick2(object sender, EventArgs e)
+        {
+            Android.App.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog alertDialog = builder.Create();
+            alertDialog.SetTitle("Alert Title");
+            alertDialog.SetIcon(Android.Resource.Drawable.IcDialogAlert);
+            alertDialog.SetMessage("This is a sample message");
+            //YES
+            alertDialog.SetButton("YES",
+                (s, ev) =>
+                {
+                    //Do Something
+                    //Only one Button Show
+                });
+            alertDialog.SetButton("No",
+               (s, ev) =>
+               {
+                   //Do Something
+                   //Only one Button Show
+               });
+            alertDialog.SetButton("Cancel",
+             (s, ev) =>
+             {
+                 //Do Something
+                 //Only one Button Show
+             });
+            alertDialog.Show();
+        }
+
+    }
+}
+
---
+++ TimeandDateDialog/TimeandDateDialog/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TimeandDateDialog
+{
+    [Activity(Label = "TimeandDateDialog", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        //DATE Picker
+        private const int DATE_DIALOG_ID = 0;
+        DateTime dateTime;
+        TextView textView1;
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var button1 = FindViewById<Button>(Resource.Id.button1);
+            textView1 = FindViewById<TextView>(Resource.Id.textView1);
+
+            dateTime = DateTime.Now;
+            textView1.Text = string.Format("{0}-{1}-{2}",
+                dateTime.Year, dateTime.Month, dateTime.Day);
+
+           
+
+            button1.Click += ButtonClick1;
+        }
+
+        private void ButtonClick1(object sender, EventArgs e)
+        {
+            ShowDialog(DATE_DIALOG_ID);
+        }
+
+        protected override Dialog OnCreateDialog(int id)
+        {
+            switch(id)
+            {
+                case DATE_DIALOG_ID:
+                var dialog=new DatePickerDialog(this,DatePickerDataSet,
+                    dateTime.Year,dateTime.Month-1,dateTime.Day);
+                return dialog;
+            }
+            return null;
+        }
+
+        private void DatePickerDataSet(object sender, DatePickerDialog.DateSetEventArgs e)
+        {
+            this.dateTime = e.Date;
+            textView1.Text = string.Format("{0}-{1}-{2}",
+                dateTime.Year, dateTime.Month, dateTime.Day);
+        }
+    }
+}
+
